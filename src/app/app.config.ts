498e88d1d908c11d1a6cac1756a440bea9cc7c39
kamula <kamulaisaac@gmail.com>
2024-05-04 15:56:08 +0300

    Configure HttpClient to use fetch API

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideAnimationsAsync()]
+  providers: [
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideHttpClient(withFetch())  // Configures HttpClient to use fetch API
+  ]
 };
