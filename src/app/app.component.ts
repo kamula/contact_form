71277576b67de169a2d1decfc1a1aea36e83ba8c
kamula <kamulaisaac@gmail.com>
2024-05-04 13:36:30 +0300

    contact form form Group configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from './contact-form/contact-form.component'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule
+  ],
   templateUrl: './contact-form.component.html',
-  styleUrl: './contact-form.component.scss'
+  styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  contactForm = new FormGroup({
+    fullName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    message: new FormControl('', [Validators.required]),
+  });
 
+  onSubmit() {
+    // Handle form submission function
+    console.log(this.contactForm.value);
+    // You can also implement an actual submission to the backend here
+  }
 }
